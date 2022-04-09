1bd1440e441d529f8693109d27afc6c8f64d8b7e
Your Name <pcismyname007@gmail.com>
2022-04-09 11:42:34 +0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Complete from './Complete.svelte'
+</script>
+
+
+<body>
+	<div class="top">
+		<img class="logo" src="img\logo.png" alt="">
+			<div class="nav_lists">		
+				<button class="nav_item">Home</button>
+				<button class="nav_item">Contact Us</button>
+				<button class="nav_item" >Help</button>
+			</div>
+	</div>
+	<Complete/>
+
+</body>
+
+
+
+
+
+
+
+
+<style>
+	
+</style>
---
+++ src/Complete.svelte
@@ -0,0 +1,60 @@
+<script>
+</script>
+
+ <img src="img/correct.png" alt="">
+    <button class="sign_out" >SIGN OUT</button>
+    <h1>ลงทะเบียนสำเร็จ</h1>
+    <button class="back">BACK TO MENU</button>
+
+<style>
+    img {
+        max-width:350px;
+        max-height:200px;
+        width: auto;
+        height: auto;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%)
+    }
+    h1{
+        margin-top: 20rem;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+
+    }
+    .sign_out {
+        background: rgba(255, 255, 255, 0);
+        display: flex;
+        position: absolute;
+        top: 2.5rem;
+        right: 3rem;
+        width: fit-content;
+        height: fit-content;
+        border-radius: 10px;
+        border: 4px solid white;  
+        color: white;
+        cursor: pointer;
+
+    }
+    .back {
+        background: rgba(255, 255, 255, 0);
+        width: 10rem;
+        height: 3rem;
+        position: absolute;
+        top: 80%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -70%);
+        border-radius: 10px;
+        border: 4px solid white;  
+        color: white;
+        font-family: Arial, Helvetica, sans-serif;
+        cursor: pointer;
+    }
+  
+</style>
