a7b63b3a3a272d3518b7a78ca85221a9877f99e0
TawanPss <99156043+TawanPss@users.noreply.github.com>
2022-04-09 14:18:34 +0700

    start1

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,11 @@
 <script>
-	import Complete from './Complete.svelte'
+import Login from './Login.svelte';
 </script>
 
 
 <body>
-	<div class="top">
-		<img class="logo" src="img\logo.png" alt="">
-			<div class="nav_lists">		
-				<button class="nav_item">Home</button>
-				<button class="nav_item">Contact Us</button>
-				<button class="nav_item" >Help</button>
-			</div>
-	</div>
-	<Complete/>
+	<Login/>
 
 </body>
 
 
-
-
-
-
-
-
-<style>
-	
-</style>
---
+++ src/Login.svelte
@@ -0,0 +1,8 @@
+<div class="top">
+    <img class="logo" src="img\logo.png" alt="">
+        <div class="nav_lists">		
+            <button class="nav_item">Home</button>
+            <button class="nav_item">Contact Us</button>
+            <button class="nav_item" >Help</button>
+        </div>
+</div>
