b72b9d8324a42354b27332b4dc0237dd634d7d73
Your Name <pcismyname007@gmail.com>
2022-04-10 14:55:05 +0700

    add event in table

--- src/Regist.svelte
+++ src/Regist.svelte
@@ -18,50 +18,46 @@
       <td>100</td>
       <td>3.0</td>
     </tr>
-    <table class="sub_table" class:selected="{current === 'sf210'}">
-      <tr >
+    <ul class="sub_table" class:selected="{current === 'sf210'}">
+      <li >
           Section 760001 Mon 9.30-12.30 Prof.Weerachai 35/100
-      </tr>
-      <tr>
+      </li>
+      <li>
         Section 740001 Tue 13.30-16.30 Prof.Phonsapsin เต็ม
-    </tr> <tr>
+      </li> <li>
       Section 760001 Mon 9.30-12.30 Prof.Tawan 21/100
 
-  </tr >
-    </table>
+      </li >
+</ul>
     <tr on:click="{() => current = 'sf220'}">
       <td>SF220</td>
       <td>INTRODUCTION TO SOFTWARE ENGINEER</td>
       <td>100</td>
       <td>3.0</td>
     </tr>
-    <table class="sub_table" class:selected="{current === 'sf220'}">
-      <tr>
-          Section 760001 Mon 9.30-12.30 Prof.Weerachai 35/100
-      </tr>
-      <tr>
-        Section 740001 Tue 13.30-16.30 Prof.Phonsapsin เต็ม
-    </tr> <tr>
-      Section 760001 Mon 9.30-12.30 Prof.Tawan 21/100
-  </tr>
 
-    </table>
+    <ul class="sub_table" class:selected="{current === 'sf220'}">
+      <li> Section 760001 Mon 9.30-12.30 Prof.Weerachai 35/100</li>  
+      <li>Section 740001 Tue 13.30-16.30 Prof.Phonsapsin เต็ม </li> 
+      <li>Section 760001 Mon 9.30-12.30 Prof.Tawan 21/100</li>
+    </ul>
+
     <tr on:click="{() => current = 'tu100'}">
       <td>TU100</td>
       <td>CIVIC ENGAGEMENT</td>
       <td>100</td>
       <td>3.0</td>
     </tr>
-    <table class="sub_table" class:selected="{current === 'tu100'}">
-      <tr>
+    <ul class="sub_table" class:selected="{current === 'tu100'}">
+      <li>
           Section 760001 Mon 9.30-12.30 Prof.Weerachai 35/100
-      </tr>
-      <tr>
+      </li>
+      <li>
         Section 740001 Tue 13.30-16.30 Prof.Phonsapsin เต็ม
-    </tr> <tr>
+      </li> <li>
       Section 760001 Mon 9.30-12.30 Prof.Tawan 21/100
-  </tr>
-    </table>
+      </li>
+</ul>
 </table>
 
 
@@ -71,17 +67,17 @@
     .main_table { 
         border-collapse: collapse; 
     }
-  
     .main_table:nth-child(2){ border: solid thin; }
     .main_table:nth-child(4) { border: solid thin; }
     .main_table:nth-child(3){ border: solid thin; }
     .sub_table{
+      position: relative;
       display: none;
     }
-
     .selected {
-      display: list-item;
-    }
+      display: inline;
+      list-style: none;
 
+    }
 
 </style>
