447152eacc702375753fdbcea3d783142fe6ba76
Your Name <pcismyname007@gmail.com>
2022-04-10 19:26:44 +0700

    table

--- src/Regist.svelte
+++ src/Regist.svelte
@@ -1,6 +1,12 @@
 <script>
+  import {accounts, account, mode} from './stores.js'
   let current = '';
-  
+  let cur_button = '';
+  let q_sf210 = 100;
+  function click(){
+    return value -= 1
+  }
+
 </script>
 
 <h1>ลงทะเบียนเรียน</h1>
@@ -15,32 +21,49 @@
     <tr  on:click="{() => current = 'sf210'}">
       <td>SF210</td>
       <td>PROGRAMMING SKILL DEVELOPEMENT 1 </td>
-      <td>100</td>
+      <td>{q_sf210}</td>
       <td>3.0</td>
     </tr>
-    <ul class="sub_table" class:selected="{current === 'sf210'}">
-      <li >
-          Section 760001 Mon 9.30-12.30 Prof.Weerachai 35/100
-      </li>
-      <li>
-        Section 740001 Tue 13.30-16.30 Prof.Phonsapsin เต็ม
-      </li> <li>
-      Section 760001 Mon 9.30-12.30 Prof.Tawan 21/100
 
-      </li >
-</ul>
+    <td colspan="4">
+    <div class="sub_table" class:selected="{current === 'sf210'}">
+      <tr class="sub_row" >
+        <td> Section 760001 </td><td>Mon 9.30-12.30</td><td>Prof.Weerachai&nbsp&nbsp&nbsp&nbsp</td>
+        <td > 35/100 &nbsp&nbsp&nbsp</td><td><button on:click="{() => current = 'check'}"  class="regist"> ลงทะเบียน </button></td>
+      </tr>
+      <tr class="sub_row">
+        <td> Section 760001 </td><td>Mon 9.30-12.30</td><td>Prof.Weerachai</td>
+        <td> 35/100 </td><td><button on:click="{() => current = 'check'}" class="regist">ลงทะเบียน</button></td>
+      </tr>
+      <tr class="sub_row">
+        <td> Section 760001 </td><td>Mon 9.30-12.30</td><td>Prof.Weerachai</td>
+        <td> 35/100 </td><td><button on:click="{() => current = 'check'}" class="regist">ลงทะเบียน</button></td>
+      </tr>
+    </div>
+  </td>
+
     <tr on:click="{() => current = 'sf220'}">
       <td>SF220</td>
       <td>INTRODUCTION TO SOFTWARE ENGINEER</td>
       <td>100</td>
       <td>3.0</td>
     </tr>
-
-    <ul class="sub_table" class:selected="{current === 'sf220'}">
-      <li> Section 760001 Mon 9.30-12.30 Prof.Weerachai 35/100</li>  
-      <li>Section 740001 Tue 13.30-16.30 Prof.Phonsapsin เต็ม </li> 
-      <li>Section 760001 Mon 9.30-12.30 Prof.Tawan 21/100</li>
-    </ul>
+    <td colspan="4">
+      <div class="sub_table" class:selected="{current === 'sf220'}">
+        <tr class="sub_row" >
+          <td> Section 760001 </td><td>Mon 9.30-12.30</td><td>Prof.Weerachai&nbsp&nbsp&nbsp&nbsp</td>
+          <td > 35/100 &nbsp&nbsp&nbsp</td><td><button  class="regist"> ลงทะเบียน </button></td>
+        </tr>
+        <tr class="sub_row">
+          <td> Section 740001 </td><td>Tue 13.30-16.30</td><td>Prof.Phonsapsin</td>
+          <td> 35/100 </td><td><button class="regist">ลงทะเบียน</button></td>
+        </tr>
+        <tr class="sub_row">
+          <td> Section 760001 </td><td>Mon 9.30-12.30</td><td>Prof.Tawan</td>
+          <td> 21/100 </td><td><button class="regist">ลงทะเบียน</button></td>
+        </tr>
+      </div>
+    </td>
 
     <tr on:click="{() => current = 'tu100'}">
       <td>TU100</td>
@@ -48,19 +71,25 @@
       <td>100</td>
       <td>3.0</td>
     </tr>
-    <ul class="sub_table" class:selected="{current === 'tu100'}">
-      <li>
-          Section 760001 Mon 9.30-12.30 Prof.Weerachai 35/100
-      </li>
-      <li>
-        Section 740001 Tue 13.30-16.30 Prof.Phonsapsin เต็ม
-      </li> <li>
-      Section 760001 Mon 9.30-12.30 Prof.Tawan 21/100
-      </li>
-</ul>
+
+    <td colspan="4">
+      <div class="sub_table" class:selected="{current === 'tu100'}">
+        <tr class="sub_row" >
+          <td> Section 760001 </td><td>Mon 9.30-12.30</td><td>Prof.Weerachai&nbsp&nbsp&nbsp&nbsp</td>
+          <td > 35/100 &nbsp&nbsp&nbsp</td><td><button  class="regist"> ลงทะเบียน </button></td>
+        </tr>
+        <tr class="sub_row">
+          <td> Section 740001 </td><td>Tue 13.30-16.30</td><td>Prof.Phonsapsin</td>
+          <td> 35/100 </td><td><button class="regist">ลงทะเบียน</button></td>
+        </tr>
+        <tr class="sub_row">
+          <td> Section 760001 </td><td>Mon 9.30-12.30</td><td>Prof.Tawan</td>
+          <td> 21/100 </td><td><button class="regist">ลงทะเบียน</button></td>
+        </tr>
+      </div>
+  </td>
+
 </table>
-
-
 
 
 <style>
@@ -70,14 +99,25 @@
     .main_table:nth-child(2){ border: solid thin; }
     .main_table:nth-child(4) { border: solid thin; }
     .main_table:nth-child(3){ border: solid thin; }
+
+    
     .sub_table{
       position: relative;
       display: none;
+      background: white;
+      border-radius: 15px;
     }
     .selected {
-      display: inline;
+      display: inline-table;
       list-style: none;
-
+      border: 15px;
     }
-
+   
+    .regist{
+      background: rgb(106, 106, 188);
+      border: none;
+      border-radius: 15px;
+      color: white;
+    }
+   
 </style>
