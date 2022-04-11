d935f112f3d8ac18109d4bf6a2fc9b45dcbbf469
TawanPss <99156043+TawanPss@users.noreply.github.com>
2022-04-11 15:52:07 +0700

    demo4

--- src/Login.svelte
+++ src/Login.svelte
@@ -15,25 +15,36 @@
             pin = '';  
         }
     }
+
+    let src = '/img/Group_34.png'; 
+     
 </script>
 
 <body> 
     
     <div class="center">
-        <h1>Enrollment Login</h1>
+        
+       <div class="system">
+            <h1>Enrollment Login</h1>
 
-        <div>
-            <label for="UserId">User ID</label>
-            <input bind:value={username}>
-        </div>
-        <div>
-            <label for="Password">Password</label>
-            <input type="password" bind:value={pin} >
-        </div>
-    
-        <div>
-        <button on:click={isLogin}>Sign in</button>
-        </div>
+            <div>
+                <label for="UserId">User ID</label>
+                <input bind:value={username}>
+            </div>
+            <div>
+                <label for="Password">Password</label>
+                <input type="password" bind:value={pin} >
+            </div>
+        
+            <div>
+            <button on:click={isLogin}>Sign in</button>
+            </div>
+        
+        </div> 
+        
+    <div class = "photo"></div>
+
+</div>
     
 </body>    
 
@@ -54,10 +65,17 @@
 
     .center{ width: 600px;
         height: 325px;
-        border: 2px solid white;
-        border-radius: 10px;
+        border: 4px solid white;
+        border-radius: 20px;
         padding: 50px 40px;
         margin-left: auto;
         margin-right: auto;
     }
+    .photo{  background-image:url(https://drive.google.com/file/d/1sFbEyW2RVCInV9JDwQAxlBRok8vLvZO6/preview);
+        background-size: cover;
+        height: 500px;
+        border-radius: 25px;
+}
+    
+    
 </style>
