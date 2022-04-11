7fc7f4f4ff663d33bc4fbbec92b7590aa3d765b2
JJSenior03 <sathit.jun03@gmail.com>
2022-04-11 15:06:32 +0700

    testing Home

---
+++ src/Home.svelte
@@ -0,0 +1,60 @@
+<script>
+    import { account,accounts,mode } from './stores.js';
+  </script>
+
+
+<body>
+ 
+  <h1>Welcome!</h1>
+  <div class="user"> {$accounts[$account].name}</div>
+  <span class = 'btn'>
+    <button  on:click={()=>$mode='regist'} class = 'btn1'>ลงทะเบียนเรียน</button><br>
+  </span>
+
+</body>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      
+  }
+  body {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+  h1 {
+    color: white;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 15rem;
+  }
+  .user {
+    color: white;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 15rem;
+  }
+  button{
+    padding: 20px 50px;
+    width: 17rem;
+    border : 0 px ;
+    border-radius: 15px;
+    border-color:white;
+    background : transparent;
+    margin: 5px;
+    
+    color: white;
+    text-align: center;
+    font-size: 1.10rem;
+  }
+  
+</style>
+
+
