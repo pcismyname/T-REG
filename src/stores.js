f03629cb3dedd5b6becd7812db94d697d78d2beb
Your Name <pcismyname007@gmail.com>
2022-04-15 22:24:22 +0700

    add more course data to JSON

--- src/stores.js
+++ src/stores.js
@@ -6,7 +6,12 @@ let data = {'0000':{name :'Chidsanuphong',surename: 'Pengchai',number: '64107420
             '3':{name :'Sathit',surename: 'Juntanasatan',number: '3', pin: '3',course : []}};
 
 
-let data_base = {'sf210_1':{quata:100,code:'sf210',sec:'760001',name:'Weerachai',surename:'Antonsorn',time:'13.00-14.00',day:'Monday'}};
+let data_base = {'sf210_1':{quata:40,code:'sf210',sub:'Programmin skill development 1',sec:'760001',name:'Weerachai',surename:'Antonsorn',time:'13.00-14.00',day:'Monday', credit:'3'},
+                'sf210_2':{quata:40,code:'sf210',sub:'Programmin skill development 1',sec:'760002',name:'Jordan',surename:'Peterson',time:'09.00-12.00',day:'Tueday', credit:'3'},
+                'sf210_3':{quata:40,code:'sf210',sub:'Programmin skill development 1',sec:'760003',name:'Ben',surename:'Shapiro',time:'13.00-14.00',day:'Wednesday', credit:'3'},
+            'sf220_1':{quata:100,code:'sf220',sub:'Introduction to software engnieer',sec:'760004',name:'Weerachai',surename:'Antonsorn',time:'13.00-14.00',day:'Thursday',credit:'3'},
+            'sf220_2':{quata:100,code:'sf220',sub:'Introduction to software engnieer',sec:'760005',name:'Ben',surename:'Shapiro',time:'13.00-14.00',day:'Friday',credit:'3'},
+            'sf220_3':{quata:100,code:'sf220',sub:'Introduction to software engnieer',sec:'760006',name:'Weerachai',surename:'Antonsorn',time:'13.00-14.00',day:'Saturday',credit:'3'},};
 
 
 export const courses = writable(data_base);
@@ -14,7 +19,7 @@ export const accounts = writable(data);
 export const islogin = writable(false);
 export const mode = writable('home');
 export const account = writable('');
-export const store_q = writable({});
+export const store_q = writable([]);
 export const temp = writable([]);
 
 
