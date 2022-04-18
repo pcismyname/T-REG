5753991cb901ce547f72467283c524d0d2240516
Your Name <pcismyname007@gmail.com>
2022-04-18 23:08:05 +0700

    add withdrawal template

--- src/Withdrawal.svelte
+++ src/Withdrawal.svelte
@@ -1,7 +1,12 @@
 <script>
-      import { account,accounts,courses,mode, temp } from './stores.js'
+      import { account,accounts,courses,mode} from './stores.js'
+
+      var keys = Object.keys($courses)
+
 </script>
 
+
+<body>
 <div class="tableConfig">
     <table class="main_table">
       <tr>
@@ -11,18 +16,29 @@
         <th>วัน</th>
         <th>เวลา</th>
     </tr>
-      {#each $accounts[account].course as item}
-          <tr>
-            <td>{$courses[item].code}</td> 
-            <td>{$courses[item].sub}</td>
-            <td>{$courses[item].credit}</td>
-            <td>{$courses[item].day}</td>
-            <td>{$courses[item].time}</td>
-            <td><button>ถอนรายวิชา</button></td>
-          </tr>
-        {/each}
+
+      {#each $accounts[$account].course as item}
+        {#if item != ''}
+        <tr>
+          <td>{$courses[item].code}</td>
+          <td>{$courses[item].sub}</td>
+          <td>{$courses[item].credit}</td>
+          <td>{$courses[item].day}</td>
+          <td>{$courses[item].time}</td>
+          <td><button>ถอนรายวิชา</button></td>
+        </tr>
+        {/if}
+    {/each}
     </table>
     </div>
+
+  </body>
+
+
+  <style>
+
+
+  </style>
 
 
 
--- src/stores.js
+++ src/stores.js
@@ -1,13 +1,13 @@
 import {writable} from 'svelte/store';
 
-let data = {'0000':{name :'Chidsanuphong',surename: 'Pengchai',number: '6410742040', pin: '0000',course : []},
+let data = {'0':{name :'Chidsanuphong',surename: 'Pengchai',number: '6410742040', pin: '0',course : []},
             '2222':{name :'Tawan',surename: 'doe',number: '2222', pin: '2222',course : []},
             '1111':{name :'Nichpa',surename: 'Pengchai',number: '1111', pin: '1111',course : []},
             '3':{name :'Sathit',surename: 'Juntanasatan',number: '3', pin: '3',course : []}};
 
 
-let data_base = {'sf210_1':{quata:0,max:30,code:'sf210',sub:'Programmin skill development 1',sec:'760001',name:'Weerachai',surename:'Antonsorn',time:'13.00-14.00',day:'Monday', credit:'3'},
-                'sf210_2':{quata:40,max:50,code:'sf210',sub:'Programmin skill development 1',sec:'760002',name:'Jordan',surename:'Peterson',time:'09.00-12.00',day:'Tueday', credit:'3'},
+let data_base = {'sf210_1':{quata:30,max:30,code:'sf210',sub:'Programmin skill development 1',sec:'760001',name:'Weerachai',surename:'Antonsorn',time:'13.00-14.00',day:'Monday', credit:'3'},
+                'sf210_2':{quata:49,max:50,code:'sf210',sub:'Programmin skill development 1',sec:'760002',name:'Jordan',surename:'Peterson',time:'09.00-12.00',day:'Tueday', credit:'3'},
                 'sf210_3':{quata:1,max:50,code:'sf210',sub:'Programmin skill development 1',sec:'760003',name:'Ben',surename:'Shapiro',time:'13.00-14.00',day:'Wednesday', credit:'3'},
             'sf220_1':{quata:10,max:50,code:'sf220',sub:'Introduction to software engnieer',sec:'860004',name:'Donna',surename:'Joe',time:'13.00-14.00',day:'Thursday',credit:'3'},
             'sf220_2':{quata:49,max:50,code:'sf220',sub:'Introduction to software engnieer',sec:'860005',name:'Bella',surename:'Dolphin',time:'13.00-14.00',day:'Friday',credit:'3'},
